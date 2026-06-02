79ccfe15143a6bb8f9a8da9b86f7dc1670dc967b
milliondreamsblog <114696765+milliondreamsblog@users.noreply.github.com>
2026-06-02 10:26:44 +0530

    feat(fe): add route guards

---
+++ Project-Managemement-Tool-FE-SGC_int_D/src/Components/ProtectedRoute.jsx
@@ -0,0 +1,23 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const ProtectedRoute = ({ allowedRoles }) => {
+    const { user } = useAuth();  
+
+
+    if (!user) {
+        return <Navigate to="/login" />;
+    }
+
+    // if (!allowedRoles.includes(user.role)) {
+    //     return <Navigate to="/" />;
+    // }
+
+    if (allowedRoles && !allowedRoles.includes(user.role)) {
+        return <Navigate to="/" />;
+    }
+
+    return <Outlet />;
+};
+
+export default ProtectedRoute;
