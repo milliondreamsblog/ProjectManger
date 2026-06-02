e750933b7c6fdab2abfd5086f6421a05e13b8f34
milliondreamsblog <114696765+milliondreamsblog@users.noreply.github.com>
2026-06-02 10:26:55 +0530

    feat(fe): add dependencies component

---
+++ Project-Managemement-Tool-FE-SGC_int_D/src/Components/Dependencies/Dependencies.jsx
@@ -0,0 +1,12 @@
+// import "./Project.css";
+
+import DependenciesTable from "../../Pages/DependenciesTable";
+const Dependencies = () => {
+  return (
+    <div className="project-page-container">
+      <DependenciesTable/>
+    </div>
+  );
+};
+
+export default Dependencies;
